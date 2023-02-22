62d3a09dabd65b634e2d812ab6ecf21add3a54a6
Mustafa <mustafa.dahodwala@agami-tech.com>
2023-02-22 17:30:12 +0530

    First Commit

---
+++ src/main/java/com/attendence/Attendence2Application.java
@@ -0,0 +1,13 @@
+package com.attendence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Attendence2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Attendence2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/attendence/Attendence2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attendence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Attendence2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
