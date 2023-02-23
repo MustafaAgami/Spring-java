dce08c99b6b4ff65d68dc2cb8f4fb6f2dde0aa4e
Mustafa <mustafa.dahodwala@agami-tech.com>
2023-02-23 10:27:07 +0530

    Added Comment

--- src/main/java/com/attendence/Attendence2Application.java
+++ src/main/java/com/attendence/Attendence2Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Attendence2Application {
 
 	public static void main(String[] args) {
+		//attendence
 		SpringApplication.run(Attendence2Application.class, args);
 	}
 
